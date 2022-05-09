8a7ae0aa0587cec7ec3d421b1de4d8e75132f7ba
Julie Nisbet <jmnisbet@gmail.com>
2022-05-09 10:15:01 -0700

    adding dog data from last week's demo

---
+++ dogs.js
@@ -0,0 +1,12 @@
+export const dogs = [
+    {
+        name: 'Benny',
+        age: 6,
+        cutenessFactor: 10,
+        image: './assets/benny.png',
+        breeds: ['Pekingese', 'Terrier', 'Chihauhau'],
+    },
+    { name: 'Jeskuh', age: 9, cutenessFactor: 8, image: './assets/jeskuh.jpeg' },
+    { name: 'Pugsly', age: 2, cutenessFactor: 9, image: './assets/pugsly.jpeg' },
+    { name: 'Larry', age: 1, cutenessFactor: 9, image: './assets/larry.jpeg' },
+];
